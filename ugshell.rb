1b60aff5961b9fa57887ed05ddaaee4ee187e57e
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2020-05-03 20:11:24 +0200

    Typo

--- ugshell.rb
+++ ugshell.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Ughub < Formula
+class Ugshell < Formula
   include Language::Python::Virtualenv
 
   desc ""
